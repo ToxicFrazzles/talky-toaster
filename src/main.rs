e6352a23b3f22275614847d5244fb1a5b2bfb0e7
toxicfrazzles <frazzles740@gmail.com>
2024-08-29 18:19:32 +0000

    Data from the radar

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::{thread::sleep, time};
+
 use esp_idf_hal::{delay::BLOCK, prelude::Peripherals};
 
+mod error;
 mod mmwave;
 
 fn main() {
@@ -10,13 +13,32 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take().unwrap();
+    // let peripherals = Peripherals::take().unwrap();
 
-    let mmtx = peripherals.pins.gpio11;
-    let mmrx = peripherals.pins.gpio10;
+    // let mmtx = peripherals.pins.gpio11;
+    // let mmrx = peripherals.pins.gpio10;
     let mut radar = mmwave::MMWave::new();
     radar.init();
-    radar.read();
+    println!("Setting report mode");
+    radar.set_mode_report().ok();
+    sleep(time::Duration::from_millis(50));
+    radar.read_frame();
 
-    log::info!("Hello, world!");
+    println!("Sending command to retrieve FW");
+    radar.read_fw_version().ok();
+    println!("Sent command to retrieve FW");
+    println!("Reading a lil bit o data");
+    loop {
+        match radar.read_frame() {
+            Ok(frame) => {
+                sleep(time::Duration::from_millis(50));
+            }
+            Err(why) => {
+                log::error!("{}", why);
+                sleep(time::Duration::from_millis(200));
+            }
+        }
+    }
+
+    // log::info!("Hello, world!");
 }
